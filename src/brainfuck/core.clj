e56bcca0f45e3f970819b60f349ccb4d2aa46424
Chris LaRose <chris.larose@mycase.com>
2015-10-18 16:52:53 -0700

    cjl - Add ast evaluator, without support for comma command yet

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -21,10 +21,35 @@
 (defn parse [program]
   (first (parse-source program)))
 
-(parse "><+--[++[--],.]-.,")
+(declare evaluate)
+
+(defn evaluate-stmt
+  "Given the current machine state and a statement,
+  yield new machine state"
+  [{:keys [tape data-pointer] :as state} stmt]
+  (case stmt
+    \> (update state :data-pointer inc)
+    \< (update state :data-pointer dec)
+    \+ (update-in state [:tape data-pointer] inc)
+    \- (update-in state [:tape data-pointer] dec)
+    \. (do
+         (print (char (nth tape data-pointer)))
+         state)
+    (loop [{:keys [tape data-pointer] :as cur-state} state]
+      (if (= (nth tape data-pointer) 0)
+        cur-state
+        (recur (evaluate cur-state stmt))))))
+
+(defn evaluate
+  [state stmts]
+  (reduce evaluate-stmt state stmts))
+
+(def initial-state
+  {:tape (apply vector-of :byte (repeat 30000 0))
+   :data-pointer 0})
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Reads brainfuck program from stdin, then executes it"
   [& args]
   (let [source (slurp *in*)]
-    (println (parse source))))
+    (evaluate initial-state (parse source))))
