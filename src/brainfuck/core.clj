cfcf0400b5997a8f3aa4bc5b32ce7a3529544a29
Chris LaRose <chris.larose@mycase.com>
2015-10-18 11:14:51 -0700

    cjl - Basic parser

--- src/brainfuck/core.clj
+++ src/brainfuck/core.clj
@@ -1,6 +1,26 @@
 (ns brainfuck.core
   (:gen-class))
 
+(def commands
+  #{\> \< \+ \- \. \,})
+
+(defn parse-source [program]
+  (loop [ast []
+         source program]
+    (let [token (first source)]
+      (cond
+       (nil? token) [ast ""]
+       (commands token) (recur
+                         (conj ast [:command token])
+                         (rest source))
+       (= token \[) (let [[loop-ast unparsed] (parse-source (rest source))]
+                      (recur (conj ast [:loop loop-ast]) unparsed))
+       (= token \]) [ast (rest source)]))))
+
+(defn parse [program]
+  (first (parse-source program)))
+
+(parse "><+--[++[--],.]-.,")
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
